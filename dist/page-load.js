515bd85394037dbfd2fd90a1f0f0872b3576c01d
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2022-12-05 17:16:28 +1030

    docs: :zap: Add javascript to page load.js
    Add variables for creating elements, append elements to dom, add classlists for styles, add content to dom variables

--- dist/page-load.js
+++ dist/page-load.js
@@ -1,5 +1,27 @@
 const pageLoad = () => {
+  // declare the variables
+  const content = document.getElementById("content");
   const container = document.createElement("div");
+  const header = document.createElement("header");
+  const img = document.createElement("img");
+  const hero = document.createElement("p");
+
+  // append the element to their respective elements on the DOM
+  content.appendChild(container);
+  container.appendChild(header);
+  container.appendChild(img);
+  container.appendChild(hero);
+
+  // Add classes to variables for styling
+  container.classList.add("container");
+  header.classList.add("header");
+  img.classList.add("img");
+  hero.classList.add("hero");
+
+  header.textContent = "The Rusty Spoon";
+  img.src = "../Assets/rusty spoon.jpg";
+  hero.textContent =
+    "For all your hunger needs, here lay the Rusty Spoon that feeds";
 };
 
 export { pageLoad };
