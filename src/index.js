ea0ced1ed412ae58fc0c62023d200273df2d88e6
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2022-12-06 17:27:32 +1030

    feat: :zap: Add event listeners for nav buttons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import { pageLoad } from "../dist/page-load";
 pageLoad();
+
+import { menuPage } from "../dist/menu-page";
+
+const homBtn = document
+  .getElementById("nav-home")
+  .addEventListener("click", () => {
+    console.log("homebtn");
+    pageLoad();
+  });
+
+const menuBtn = document
+  .getElementById("nav-menu")
+  .addEventListener("click", () => {
+    console.log("menuebtn");
+    menuPage();
+  });
+
+const contactBtn = document
+  .getElementById("nav-contact")
+  .addEventListener("click", () => {
+    console.log("contactbtn");
+  });
