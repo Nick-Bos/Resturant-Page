7d476890e20fdf1501f793db82016f558122c5ad
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2022-12-13 17:30:19 +1030

    refactor: :fire: Remove navbar related code

--- dist/menu-page.js
+++ dist/menu-page.js
@@ -1,10 +1,6 @@
 const menuPage = () => {
   // declare the variables
   const content = document.getElementById("content");
-  const navBar = document.createElement("nav");
-  const home = document.createElement("button");
-  const menu = document.createElement("button");
-  const contact = document.createElement("button");
   const container = document.createElement("div");
   const mealOne = document.createElement("div");
   const mealOneTitle = document.createElement("h2");
@@ -12,10 +8,6 @@ const menuPage = () => {
 
   // append the element to their respective elements on the DOM
   content.appendChild(container);
-  container.appendChild(navBar);
-  navBar.appendChild(home);
-  navBar.appendChild(menu);
-  navBar.appendChild(contact);
   container.appendChild(mealOne);
   mealOne.appendChild(mealOneTitle);
   mealOne.appendChild(mealOneInfo);
