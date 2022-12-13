fdba7f5119141ca03db8be22ed5300c6f467d626
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2022-12-13 17:31:25 +1030

    docs: :zap: Add clearContent function and complete the index file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+import { NavBar } from "../dist/nav-bar";
 import { pageLoad } from "../dist/page-load";
 import { menuPage } from "../dist/menu-page";
-import { NavBar } from "../dist/nav-bar";
+import { contactPage } from "../dist/contact-page";
 
 //init page function calls
 NavBar();
 pageLoad();
+
+//Tab swithing functions
+function clearContent() {
+  const contents = document.getElementById("content");
+  const remove = contents.childNodes[1];
+  remove.remove();
+}
 
 //navbar button event listener functions
 const homBtn = document
   .getElementById("nav-home")
   .addEventListener("click", () => {
     console.log("homebtn");
+    clearContent();
     pageLoad();
   });
 
@@ -18,6 +27,7 @@ const menuBtn = document
   .getElementById("nav-menu")
   .addEventListener("click", () => {
     console.log("menuebtn");
+    clearContent();
     menuPage();
   });
 
@@ -25,4 +35,6 @@ const contactBtn = document
   .getElementById("nav-contact")
   .addEventListener("click", () => {
     console.log("contactbtn");
+    clearContent();
+    contactPage();
   });
